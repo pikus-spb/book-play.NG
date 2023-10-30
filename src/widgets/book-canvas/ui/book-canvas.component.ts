6732e86ed4cb87a1f3b2c5ae9a42914c0e21c368
Petr Savelev <petr.savelev.direct@gmail.com>
2023-10-30 16:28:12 +0300

    BOOK-37: Create POC functionality for auto scroll book to cursor - // implemented a working POC

--- src/widgets/book-canvas/ui/book-canvas.component.ts
+++ src/widgets/book-canvas/ui/book-canvas.component.ts
@@ -1,10 +1,21 @@
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import {
+  CdkVirtualScrollViewport,
+  ScrollingModule,
+} from '@angular/cdk/scrolling';
 import { ScrollingModule as ExperimentalScrollingModule } from '@angular/cdk-experimental/scrolling';
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 
+import { delay, Subject, takeUntil, tap } from 'rxjs';
 import { BookParagraphComponent } from 'src/features/book-paragraph';
 import { Author } from 'src/entities/fb2';
-import { MaterialModule } from 'src/shared/ui';
+import { LoadingService, MaterialModule } from 'src/shared/ui';
+const INDEX = 1000;
 
 @Component({
   selector: 'book-canvas',
@@ -18,9 +29,64 @@ import { MaterialModule } from 'src/shared/ui';
     BookParagraphComponent,
   ],
 })
-export class BookCanvasComponent {
+export class BookCanvasComponent implements AfterViewInit {
   @Input() paragraphs: string[] = [];
   @Input() author: Author = {} as Author;
   @Input() bookTitle = '';
   @Input() bookTitlePicture: string | null = '';
+  @ViewChild('scrollViewport') scrollViewport!: CdkVirtualScrollViewport;
+
+  private scrolledToIndex$ = new Subject<void>();
+
+  constructor(
+    private el: ElementRef,
+    public loadingService: LoadingService
+  ) {}
+
+  private scrollToLastElement() {
+    const paragraph = this.el.nativeElement.querySelector(
+      `book-paragraph:last-of-type`
+    );
+    if (paragraph) {
+      paragraph.scrollIntoView();
+    }
+  }
+
+  private scrollToElement(query: string, options: any = undefined) {
+    const element = this.el.nativeElement.querySelector(query);
+    if (element) {
+      element.scrollIntoView(options);
+    }
+  }
+
+  private scrollToIndex(index: number) {
+    const range = this.scrollViewport.getRenderedRange();
+    console.log(range);
+    if (index >= range.start && index <= range.end) {
+      this.scrollToElement(
+        `book-paragraph:nth-of-type(${INDEX - range.start})`,
+        { block: 'center' }
+      );
+      this.scrolledToIndex$.next();
+      this.loadingService.loading = false;
+    } else {
+      this.scrollToElement('book-paragraph:last-of-type');
+    }
+  }
+
+  ngAfterViewInit() {
+    console.log(this.paragraphs[1000]);
+
+    this.loadingService.loading = true;
+    this.scrollViewport.scrollable
+      .elementScrolled()
+      .pipe(
+        delay(1),
+        takeUntil(this.scrolledToIndex$),
+        tap(() => this.scrollToIndex(INDEX))
+      )
+      .subscribe();
+
+    setTimeout(() => this.scrollToIndex(INDEX));
+  }
 }
