806ff6a66df726ebc225877b0a8c657e1bbdf7a6
Petr Savelev <petr.savelev.direct@gmail.com>
2023-11-11 02:57:54 +0300

    BOOK-46: Loading service is not working properly - // сделал

--- src/shared/ui/loading/loading.service.ts
+++ src/shared/ui/loading/loading.service.ts
@@ -1,21 +1,46 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, shareReplay } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { filter, Observable, Subject, takeUntil, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoadingService {
-  private _loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-    false
-  );
-
-  public loading$: Observable<boolean> = this._loading$.pipe(shareReplay(1));
+export class LoadingService implements OnDestroy {
+  private _loading$: Subject<boolean> = new Subject<boolean>();
+  private _destroyed$: Subject<void> = new Subject<void>();
 
   set loading(loading: boolean) {
     this._loading$.next(loading);
   }
 
-  get loading(): boolean {
-    return this._loading$.value;
+  public loading$: Observable<boolean> = new Observable<boolean>(subscriber => {
+    let loadingIterationsCount = 0;
+
+    this._loading$
+      .pipe(
+        filter((loading: boolean) => loading),
+        tap(() => {
+          loadingIterationsCount++;
+          subscriber.next(true);
+        }),
+        takeUntil(this._destroyed$)
+      )
+      .subscribe();
+    this._loading$
+      .pipe(
+        filter((loading: boolean) => !loading),
+        tap(() => {
+          loadingIterationsCount--;
+          if (loadingIterationsCount <= 0) {
+            subscriber.next(false);
+            loadingIterationsCount = 0;
+          }
+        }),
+        takeUntil(this._destroyed$)
+      )
+      .subscribe();
+  });
+
+  ngOnDestroy() {
+    this._destroyed$.next();
   }
 }
