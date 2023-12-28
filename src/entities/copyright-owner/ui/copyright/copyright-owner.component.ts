8bc51c1e70a2b5081991773b0788d495b383e790
Petr Savelev <petr.savelev.direct@gmail.com>
2023-12-28 22:10:27 +0300

    Переработал ссылку mailto:

--- src/entities/copyright-owner/ui/copyright/copyright-owner.component.ts
+++ src/entities/copyright-owner/ui/copyright/copyright-owner.component.ts
@@ -7,4 +7,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class CopyrightOwnerComponent {}
+export class CopyrightOwnerComponent {
+  public href: string = [
+    'mailto:pikus@post.com?',
+    'subject=',
+    'Нарушение авторских прав',
+    '&body=',
+    'Добрый день, Петр Савельев!%0A',
+    `Прошу удалить книгу, нарушающую авторские права:%0A
+    
+    %0A<укажите название>%0A
+
+    %0A<укажите претензию>%0A
+
+    %0A(Если ваши авторские права будут подтверждены, книга будет удалена в течении 2 недель, спасибо за понимание)`,
+  ].join('');
+}
