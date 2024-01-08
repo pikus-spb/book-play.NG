46bce0b8320aba9143904e988629bace7cc35c89
Petr Savelev <petr.savelev.direct@gmail.com>
2024-01-09 00:13:27 +0300

    Сделал разбивку библиотеки по буквам

--- src/pages/library/ui/library/library.component.ts
+++ src/pages/library/ui/library/library.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable, reduce, tap } from 'rxjs';
 
 import { BooksApiService } from 'src/entities/books/';
 import { Events, EventsStateService } from 'src/shared/ui';
@@ -21,13 +21,24 @@ export class LibraryComponent {
     private eventStates: EventsStateService
   ) {}
 
-  public getAllBooks(): Observable<BookDescription[]> {
+  public getAllBookLetters(): Observable<Record<string, BookDescription[]>> {
     this.eventStates.add(Events.loading, true);
+
     return this.api.getAll().pipe(
       map((books: BookDescription[]) => {
         return books.sort((a, b) => {
           return a.authorLastName.localeCompare(b.authorLastName);
         });
+      }),
+      map((books: BookDescription[]) => {
+        return books.reduce(
+          (memo: Record<string, BookDescription[]>, item: BookDescription) => {
+            memo[item.authorLastName[0]] = memo[item.authorLastName[0]] || [];
+            memo[item.authorLastName[0]].push(item);
+            return memo;
+          },
+          {} as Record<string, BookDescription[]>
+        );
       }),
       tap(() => {
         this.eventStates.add(Events.loading, false);
